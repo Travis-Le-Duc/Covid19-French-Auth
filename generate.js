081bb83ef3be13afb5d5b17fa0ece1cd1747011a
Travis <travis@MacBook-Pro-de-Travis.local>
2020-11-27 16:03:41 +0100

    Update certificate and somes words

--- generate.js
+++ generate.js
@@ -43,7 +43,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 function badScriptLoading(event) {
     console.log("Official JS is not working. Patching using local copy.")
     let script = document.createElement('script');
-    script.src = "deplacement-covid-19/main.02381a6a.js";
+    script.src = "deplacement-covid-19/main.90bd2118.js";
     document.head.append(script)
 }
 
